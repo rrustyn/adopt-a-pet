43c72cadef40af53d8675e917f7b9b2cc21d0550
Ryan Rustyn <ryanrustyn@gmail.com>
2022-08-05 12:24:20 -0700

    create Pet table model

---
+++ models.py
@@ -0,0 +1,33 @@
+"""Models for adopt app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+class Pet(db.Model):
+    """Pets table"""
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+
+    name = db.Column(db.String(50), nullable = False)
+
+    species = db.Column(db.String(50), nullable = False)
+
+    photo_url = db.Column(db.Text, nullable = False, default = "")
+
+    age = db.Column(db.String(10), nullable = False) #verification on db side?
+
+    notes = db.Column(db.Text)
+
+    available = db.Column(db.Boolean, nullable = False, default = True)
