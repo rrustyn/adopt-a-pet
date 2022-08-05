878139d651b8011aa73d262ca24012feb64057c6
Alex <alexjooho@gmail.com>
2022-08-05 15:46:08 -0700

    Finished Up To Step 9

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
 
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
 
 from flask_wtf import FlaskForm
 
@@ -68,3 +68,23 @@ def add_pet():
     else:
         return render_template(
             "add-pet.html", form=form)
+
+
+@app.route('/<int:pet_id>', methods=['GET', 'POST'])
+def display_info_and_handle_edit(pet_id): #could just call edit_pet
+    """ Show information about pet and handle edits """
+
+    pet = Pet.query.get_or_404(pet_id)
+
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+        flash(f"{pet.name} updated!")
+        return redirect(f"/{pet_id}")
+
+    else:
+        return render_template("edit-pet.html", form=form, pet=pet) #don't forget pet=pet
--- forms.py
+++ forms.py
@@ -2,19 +2,22 @@ from ast import In
 from tokenize import String
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, SelectField, TextAreaField
+from wtforms import StringField, SelectField, TextAreaField, BooleanField
 
 from wtforms.validators import InputRequired, Optional, URL
 
 """Forms for adopt app."""
 
 class AddPetForm(FlaskForm):
+    """ Form for adding a new pet """
 
     name = StringField("Pet name: ",
                         validators=[InputRequired()])
 
-    species = StringField("Species: ",
-                           validators=[InputRequired()])
+    species = SelectField("Species: ", choices=[("cat", "Cat"),
+                                        ("dog", "Dog"),
+                                        ("porcupine", "Porcupine")],
+                        validators=[InputRequired()])
 
     photo_url = StringField("Photo URL: ",
                              validators=[Optional(), URL()])
@@ -26,3 +29,13 @@ class AddPetForm(FlaskForm):
                         validators=[InputRequired()])
 
     notes = TextAreaField("Notes: ", validators=[Optional()])
+
+class EditPetForm(FlaskForm):
+    """ Form for editing an existing pet """
+
+    photo_url = StringField("Photo URL: ",
+                             validators=[Optional(), URL()])
+
+    notes = TextAreaField("Notes: ", validators=[Optional()]) # if doing optional, add length or something
+
+    available = BooleanField("Available: ")
